70ff007b2958472fe66f2e1ec37acd08062eadf9
Xueping <pengxueping@gmail.com>
2016-12-22 14:41:25 +1100

    combine two RDD data into one

--- src/main/scala/clustering/report/clustering/ClusteringAPCData .scala
+++ src/main/scala/clustering/report/clustering/ClusteringAPCData .scala
@@ -15,9 +15,9 @@ class ClusteringAPCData {
 
     // Initialize Spark configuration & context
     val sparkConf: SparkConf = new SparkConf().setAppName(appName)
-      .setMaster("local[2]")
+      .setMaster("local[10]")
 //      .set("spark.executor.memory", "2gb")
-      .set("spark.driver.memory","6gb")
+      .set("spark.driver.memory","60gb")
           .set("spark.rdd.compress","true")
 //          .set("spark.memory.useLegacyMode","true")
           .set("spark.storage.memoryFraction", "0.9")
@@ -29,54 +29,52 @@ class ClusteringAPCData {
 
     val sc: SparkContext = new SparkContext(sparkConf);
 
-    val rawData = sc.textFile("/Users/xuepingpeng/Dropbox/team/HoD/interactiveClustering/demo_pre.csv").cache()
+    val rawData = sc.textFile("./resource/source/demo_pre.csv").cache()
+//    rawData
+    
     val dataAndLabel = vectorization(rawData)
     
     
 //    val data = dataAndLabel.map(_._1)
     
     dataAndLabel.map(_._1)
-    
-    
-
-
   }
 
-  // Loads and parses data
-  def parse(line: String): Vector = Vectors.dense(line.split(" ").map(_.toDouble))
 
   def vectorization(normalizedData: RDD[String]) = {
 
-    val ethnicities = normalizedData.map(_.split(",")(2)).distinct.collect.zipWithIndex.toMap
-    val payors = normalizedData.map(_.split(",")(3)).distinct.collect.zipWithIndex.toMap
-    val religions = normalizedData.map(_.split(",")(4)).distinct.collect.zipWithIndex.toMap
-    val admissions = normalizedData.map(_.split(",")(5)).distinct.collect.zipWithIndex.toMap
+    val ethnicities = normalizedData.map(_.split(",")(1)).distinct.collect.zipWithIndex.toMap
+    val payors      = normalizedData.map(_.split(",")(2)).distinct.collect.zipWithIndex.toMap
+    val admissions  = normalizedData.map(_.split(",")(3)).distinct.collect.zipWithIndex.toMap
 
 //    val dataAndLabel = 
       normalizedData.map {
-      line =>
+      line =>{
         val buffer = ArrayBuffer[String]()
 
         buffer.appendAll(line.split(","))
-        val ethnicity = buffer.remove(2)
-        val payor = buffer.remove(3)
-        val religion = buffer.remove(4)
-        val admission = buffer.remove(5)
-        val label = buffer.remove(1)
-        val vector = buffer.map(_.toDouble)
+        
+        val label     = buffer.remove(0)
+        val ethnicity = buffer.remove(0)
+        val payor     = buffer.remove(0)
+        val admission = buffer.remove(0)
+        
+        val vector    = buffer.map(_.toDouble)
+        
         val newEthnicityFeatures = new Array[Double](ethnicities.size)
         newEthnicityFeatures(ethnicities(ethnicity)) = 1.0
+        
         val newPayorFeatures = new Array[Double](payors.size)
         newPayorFeatures(payors(payor)) = 1.0
-        val newReligionFeatures = new Array[Double](religions.size)
-        newReligionFeatures(religions(religion)) = 1.0
+        
         val newAdmissionFeatures = new Array[Double](admissions.size)
-        newReligionFeatures(admissions(admission)) = 1.0
-        vector.insertAll(1, newEthnicityFeatures)
-        vector.insertAll(1, newPayorFeatures)
-        vector.insertAll(1, newReligionFeatures)
-        vector.insertAll(1, newAdmissionFeatures)
-        (vector.toArray, label)
+        newAdmissionFeatures(admissions(admission)) = 1.0
+        
+        vector.insertAll(0, newAdmissionFeatures)
+        vector.insertAll(0, newPayorFeatures)
+        vector.insertAll(0, newEthnicityFeatures)
+        (Vectors.dense(vector.toArray), label)
+      }
     }
 
   }
